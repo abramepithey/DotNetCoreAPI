ac2af48fee0ff1100e2339140805861b79b48d9d
abramepithey <abramepithey@gmail.com>
2021-04-21 07:29:21 -0600

    Adds dependency injection to ProductsController

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -11,6 +11,15 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+        
         [HttpGet]
         public string GetProducts()
         {
