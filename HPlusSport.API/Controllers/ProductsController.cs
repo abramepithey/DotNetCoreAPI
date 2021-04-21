28f2a4242028b99e1d02abb4234e8a8cd2fa5901
abramepithey <abramepithey@gmail.com>
2021-04-21 07:30:52 -0600

    Adds using statements to the controller

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HPlusSport.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
