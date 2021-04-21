173dbf1196498242edd378c521a75250cc3026ba
abramepithey <abramepithey@gmail.com>
2021-04-21 07:30:38 -0600

    Adds the GetProduct method

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -25,5 +25,14 @@ namespace HPlusSport.API.Controllers
         {
             return Ok(await _context.Products.ToArrayAsync());
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
     }
 }
