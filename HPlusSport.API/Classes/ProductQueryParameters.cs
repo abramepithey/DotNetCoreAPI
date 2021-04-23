0373ffec0d4a4938d0c86de88f4efde54b511289
abramepithey <abramepithey@gmail.com>
2021-04-23 07:32:18 -0600

    Adds Name property to QueryParameters

--- HPlusSport.API/Classes/ProductQueryParameters.cs
+++ HPlusSport.API/Classes/ProductQueryParameters.cs
@@ -5,5 +5,7 @@ namespace HPlusSport.API.Classes
         public string Sku { get; set; }
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
+
+        public string Name { get; set; }
     }
 }
