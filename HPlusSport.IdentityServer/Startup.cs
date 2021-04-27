55e828e6828254465469a6433b5517a934af50d9
abramepithey <abramepithey@gmail.com>
2021-04-27 17:25:03 -0600

    Adds and configures IdentityServer to the services in Startup.cs

--- HPlusSport.IdentityServer/Startup.cs
+++ HPlusSport.IdentityServer/Startup.cs
@@ -16,6 +16,12 @@ namespace HPlusSport.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = services.AddIdentityServer()
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.Scopes);
+
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +32,7 @@ namespace HPlusSport.IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            app.UseIdentityServer();
         }
     }
 }
