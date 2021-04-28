ec5b79e4262c0cbe2fada860547794e020c46574
abramepithey <abramepithey@gmail.com>
2021-04-27 18:06:16 -0600

    Adds Console App project to the solution

---
+++ HPlusSport.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HPlusSport.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
